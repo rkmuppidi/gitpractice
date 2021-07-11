31620d159735881a083e6a96e893faf30c4f9570
Ravikiran Muppidi <muppidi4all@gmail.com>
2021-07-11 18:20:07 +0530

    Adding name.py file

---
+++ name.py
@@ -0,0 +1,10 @@
+myName='Ravi'
+myAge=69
+myHeight='6 Feet'
+myWeight=75
+myHair='Black'
+print(f"Let's talk about {myName}.")
+print(f"Let's talk about {myAge}.")
+print(f"Let's talk about {myHeight}.")
+print(f"Let's talk about {myWeight}.")
+print(f"Let's talk about {myHair}.")
